b3082ac1f936fc69565da633313a8269adc89497
psycho-baller <Ramim66809@gmail.com>
2022-08-11 16:40:57 +0400

    A leetcode a day keeps the uneployment away

--- 054.longest-repeating-character-replacement.py
+++ 054.longest-repeating-character-replacement.py
@@ -5,8 +5,26 @@
 #
 
 # @lc code=start
+import collections
+
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        
+        max_count = 0
+        max_f = 0
+        l = 0
+        char_to_amount = {}
+        for r in range(len(s)):
+            char_to_amount[s[r]] = 1 + char_to_amount.get(s[r],0)
+            max_f = max(max_f, char_to_amount[s[r]])
+
+            while r-l+1 - max_f > k:
+                char_to_amount[s[l]] -= 1
+                l += 1
+
+            max_count = max(r - l + 1, max_count)
+
+        return max_count
+
 # @lc code=end
 
