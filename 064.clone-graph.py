f543e23ee26e369384f2eb9218d9c54a3a8ce19c
Rami Maalouf <rami.rami@ucalgary.ca>
2024-12-23 14:49:13 +0400

    weird q I gotta do again

---
+++ 064.clone-graph.py
@@ -0,0 +1,44 @@
+#
+# @lc app=leetcode id=133 lang=python3
+#
+# [133] Clone Graph
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+"""
+
+
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+from typing import Optional
+
+
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return None
+
+        visited = {}
+
+        def dfs(node):
+            if node in visited:
+                return visited[node]
+
+            clone = Node(node.val)
+            visited[node] = clone
+
+            for neighbor in node.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+
+            return clone
+
+        return dfs(node)
+
+
+# @lc code=end
