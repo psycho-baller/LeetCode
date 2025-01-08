3ce1358dde60a5307d5c6f339f79b266afda33d0
Rami Maalouf <rami.rami@ucalgary.ca>
2025-01-08 11:34:15 +0400

    two sum 2

--- 018.two-sum-ii-input-array-is-sorted.py
+++ 018.two-sum-ii-input-array-is-sorted.py
@@ -10,21 +10,31 @@ from typing import List
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        def condition(num, mid) -> bool:
-            if num + numbers[mid] > target:
-                return True
-            return False
+        left, right = 0, len(numbers) - 1
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            elif numbers[left] + numbers[right] < target:
+                left += 1
+            else:
+                right -= 1
 
-        # for num in numbers:
-        for i in range(len(numbers)):
-            left, right = i+1, len(numbers)
-            while left < right:
-                mid = left + (right - left) // 2
-                if condition(numbers[i], mid):
-                    right = mid
-                else:
-                    left = mid + 1
-            if numbers[i] + numbers[left - 1] == target:
-                return [i+1, left]
+        # def condition(num, mid) -> bool:
+        #     if num + numbers[mid] > target:
+        #         return True
+        #     return False
+
+        # # for num in numbers:
+        # for i in range(len(numbers)):
+        #     left, right = i+1, len(numbers)
+        #     while left < right:
+        #         mid = left + (right - left) // 2
+        #         if condition(numbers[i], mid):
+        #             right = mid
+        #         else:
+        #             left = mid + 1
+        #     if numbers[i] + numbers[left - 1] == target:
+        #         return [i+1, left]
+
+
 # @lc code=end
-
