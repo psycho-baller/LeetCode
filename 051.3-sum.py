269c939a6edf716b5362876db634772838500942
Rami Maalouf <rami.rami@ucalgary.ca>
2025-01-08 11:07:46 +0400

    solved 3sum again with more effiiency this time

--- 051.3-sum.py
+++ 051.3-sum.py
@@ -6,28 +6,55 @@
 
 # @lc code=start
 
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        len_nums = len(nums)
-        ans = []
         nums.sort()
-        
-        for i,v in enumerate(nums):
-            if i>0 and v == nums[i - 1]:
+        res = []
+        len_nums = len(nums)
+
+        for i in range(len_nums):
+            # if the current number is the same as the previous number, skip it
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            left, right = i + 1, len_nums -1
-            while right > left:
-                if v + nums[left] + nums[right] > 0:
-                    right-=1
-                elif v + nums[left] + nums[right] < 0:
+            # two pointers
+            left, right = i + 1, len_nums - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total < 0:
                     left += 1
+                elif total > 0:
+                    right -= 1
                 else:
-                    ans.append([v, nums[left], nums[right]])
-                    left+=1
-                    while nums[left] == nums[left-1] and left < right:
-                        left+=1
-        return ans # O(n^2) time | O(n,1) space
-    
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+        return res
+
+        # len_nums = len(nums)
+        # ans = []
+        # nums.sort()
+
+        # for i,v in enumerate(nums):
+        #     if i>0 and v == nums[i - 1]:
+        #         continue
+        #     left, right = i + 1, len_nums -1
+        #     while right > left:
+        #         if v + nums[left] + nums[right] > 0:
+        #             right-=1
+        #         elif v + nums[left] + nums[right] < 0:
+        #             left += 1
+        #         else:
+        #             ans.append([v, nums[left], nums[right]])
+        #             left+=1
+        #             while nums[left] == nums[left-1] and left < right:
+        #                 left+=1
+        # return ans # O(n^2) time | O(n,1) space
+
         # O(n^3) time | O(n,1) space
         # one = 0
         # while one < len_nums-2:
@@ -42,5 +69,6 @@ class Solution:
         #         two+=1
         #     one+=1
         # return ans
-# @lc code=end
 
+
+# @lc code=end
