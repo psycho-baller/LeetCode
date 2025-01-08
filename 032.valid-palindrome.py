ac40141618406dcc35f4a583c99246b2d2329b36
Rami Maalouf <rami.rami@ucalgary.ca>
2025-01-08 10:31:39 +0400

    fix tiny bug

--- 032.valid-palindrome.py
+++ 032.valid-palindrome.py
@@ -7,21 +7,10 @@
 
 # @lc code=start
 class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        # O(n) time and O(n) space
-        strings = [string.lower() for string in s if string.isalnum()]
-        left, right = 0, len(strings) - 1
-        while left < right:
-            if strings[left] == strings[right]:
-                left += 1
-                right -= 1
-            else:
-                return False
-        return True
 
     def isPalindrome(self, s: str) -> bool:
         # O(n) time and O(1) space
-        left, right = 0, len(strings) - 1
+        left, right = 0, len(s) - 1
         while left < right:
             if s[left].isalnum() and s[right].isalnum():
                 if s[left].lower() != s[right].lower():
@@ -33,6 +22,18 @@ class Solution:
             elif not s[right].isalnum():
                 right -= 1
         return True
+
+    # def isPalindrome(self, s: str) -> bool:
+    #     # O(n) time and O(n) space
+    #     strings = [string.lower() for string in s if string.isalnum()]
+    #     left, right = 0, len(strings) - 1
+    #     while left < right:
+    #         if strings[left] == strings[right]:
+    #             left += 1
+    #             right -= 1
+    #         else:
+    #             return False
+    #     return True
 
 
 # @lc code=end
