8db6608f484e3dd1c8d90a6e61401bb240227682
Rami Maalouf <rami.rami@ucalgary.ca>
2025-01-08 10:20:15 +0400

    more space efficient sulution

--- 032.valid-palindrome.py
+++ 032.valid-palindrome.py
@@ -4,9 +4,11 @@
 # [125] Valid Palindrome
 #
 
+
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        # O(n) time and O(n) space
         strings = [string.lower() for string in s if string.isalnum()]
         left, right = 0, len(strings) - 1
         while left < right:
@@ -16,5 +18,21 @@ class Solution:
             else:
                 return False
         return True
-# @lc code=end
 
+    def isPalindrome(self, s: str) -> bool:
+        # O(n) time and O(1) space
+        left, right = 0, len(strings) - 1
+        while left < right:
+            if s[left].isalnum() and s[right].isalnum():
+                if s[left].lower() != s[right].lower():
+                    return False
+                left += 1
+                right -= 1
+            elif not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+        return True
+
+
+# @lc code=end
