20253413abdd87e247b380a04115c62e3305777a
Rami Maalouf <rami.rami@ucalgary.ca>
2025-01-09 11:40:50 +0400

    clean sltn

--- 067.string-compression.py
+++ 067.string-compression.py
@@ -8,43 +8,20 @@
 # @lc code=start
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        idx = 1
-        count = 1
-        current_letter_idx = 0
-        current_letter = chars[current_letter_idx]
+        idx = 0
+        res = 0
         while idx < len(chars):
-            if chars[idx] == current_letter:
+            count = 1
+            while idx + count < len(chars) and chars[idx + count] == chars[idx]:
                 count += 1
-                if idx != len(chars) - 1:
-                    idx += 1
-                    continue
-                if count > 1:
-                    chars[current_letter_idx + 1 : idx + 1] = list(str(count))
-                break
-            print(
-                "current_letter",
-                current_letter,
-                "count",
-                count,
-                "idx",
-                idx,
-                "current_letter_idx",
-                current_letter_idx,
-            )
-            print("old chars", chars)
-            # loop through current_letter_idx+1 -> idx and remove all the chars and replace with count
+            chars[res] = chars[idx]
+            res += 1
             if count > 1:
-                chars[current_letter_idx + 1 : idx] = list(str(count))
-                current_letter_idx = idx - (count - 2)
-                idx = current_letter_idx + 1
-                count = 1
-            else:
-                current_letter_idx = idx
-                len_list = len(chars)
-                idx += 1
-            current_letter = chars[current_letter_idx]
-            print("chars", chars)
-        return len(chars)
+                str_rep = str(count)
+                chars[res : res + len(str_rep)] = list(str_rep)
+                res += len(str_rep)
+            idx += count
+        return res
 
 
 # @lc code=end
