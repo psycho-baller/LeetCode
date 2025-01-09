98f84822ee2203f68017cab0a17bbe6c0dfb260d
Rami Maalouf <rami.rami@ucalgary.ca>
2025-01-09 11:59:23 +0400

    simpler way of solving it

--- 024.remove-duplicates-from-sorted-array.py
+++ 024.remove-duplicates-from-sorted-array.py
@@ -10,18 +10,29 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        duplicates = 0
-        i = 1
-        nums_len = len(nums)
-        while (i + duplicates) < nums_len:
-            if nums[i] == nums[i - 1]:
-                nums.append(nums.pop(i))
-                duplicates+=1
+
+        # remove duplicates
+        idx = 1
+        while idx < len(nums):
+            if nums[idx - 1] == nums[idx]:
+                nums.pop(idx)
+                # duplicates += 1
             else:
-                i+=1
-        non_duplicates = nums_len - duplicates
-        return non_duplicates
+                idx += 1
+        return len(nums)
+
+        # count duplicates then subtract it from it's length to find the len of unique
+        # duplicates = 0
+        # i = 1
+        # nums_len = len(nums)
+        # while (i + duplicates) < nums_len:
+        #     if nums[i] == nums[i - 1]:
+        #         nums.append(nums.pop(i))
+        #         duplicates+=1
+        #     else:
+        #         i+=1
+        # non_duplicates = nums_len - duplicates
+        # return non_duplicates
 
 
 # @lc code=end
-
