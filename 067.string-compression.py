0508d994110960b79eba5ffa75e5b142bd7137a0
Rami Maalouf <rami.rami@ucalgary.ca>
2025-01-10 09:43:23 +0400

    string compression beaten

--- 067.string-compression.py
+++ 067.string-compression.py
@@ -8,20 +8,38 @@
 # @lc code=start
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        idx = 0
+        i = 0
         res = 0
-        while idx < len(chars):
-            count = 1
-            while idx + count < len(chars) and chars[idx + count] == chars[idx]:
-                count += 1
-            chars[res] = chars[idx]
+        while i < len(chars):
+            grouped_letters = 1
+            while (
+                i + grouped_letters < len(chars)
+                and chars[i] == chars[i + grouped_letters]
+            ):
+                grouped_letters += 1
+            chars[res] = chars[i]
             res += 1
-            if count > 1:
-                str_rep = str(count)
-                chars[res : res + len(str_rep)] = list(str_rep)
-                res += len(str_rep)
-            idx += count
+            if grouped_letters > 1:
+                str_repr = str(grouped_letters)
+                chars[res : res + len(str_repr)] = list(str_repr)
+                res += len(str_repr)
+            i += grouped_letters
         return res
+
+        # idx = 0
+        # res = 0
+        # while idx < len(chars):
+        #     count = 1
+        #     while idx + count < len(chars) and chars[idx + count] == chars[idx]:
+        #         count += 1
+        #     chars[res] = chars[idx]
+        #     res += 1
+        #     if count > 1:
+        #         str_rep = str(count)
+        #         chars[res : res + len(str_rep)] = list(str_rep)
+        #         res += len(str_rep)
+        #     idx += count
+        # return res
 
 
 # @lc code=end
